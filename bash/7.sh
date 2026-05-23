6d64af58f7c1417e365a124e869ed1cd33b62a91
Schcerbakov Maxim <miko.r4r@gmail.com>
2026-05-23 11:29:19 +0300

    fix: 7 sh

--- bash/7.sh
+++ bash/7.sh
@@ -3,11 +3,8 @@
 IFS=':' read -r -a path_entries <<< "$PATH"
 
 for entry in "${path_entries[@]}"; do
-    if [ -d "$entry" ]; then
-        count=$(find "$entry" -maxdepth 1 -type f | wc -l)
-    else
-        count=0
-    fi
+    [ -d "$entry" ] || continue
+    count=$(find "$entry" -maxdepth 1 -type f | wc -l)
 
     printf '%s=>%s\n' "$entry" "$count"
 done
